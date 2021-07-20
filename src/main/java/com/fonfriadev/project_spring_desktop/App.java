49c65ccdc20e0895bc18ebd9836886be5f082748
danie <leinadfonfria@gmail.com>
2021-07-20 10:17:16 +0200

    Proyecto: project-spring-desktop

---
+++ src/main/java/com/fonfriadev/project_spring_desktop/App.java
@@ -0,0 +1,13 @@
+package com.fonfriadev.project_spring_desktop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
